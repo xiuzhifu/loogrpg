7f73a9365b7d3ebb58e6c494ca6c41a2f8ff1313
suisuiai <441414288@qq.com>
2015-01-12 16:29:45 +0800

    -*.log

---
+++ frameworks/runtime-src/Classes/src/lmap.h
@@ -0,0 +1,15 @@
+#ifndef LMAP_H
+#define LMAP_H
+
+#if __cplusplus
+extern "C" {
+#endif
+#include <lua.h>
+int open_map(lua_State *L);
+#if __cplusplus
+} // extern "C"
+#endif
+
+#endif
+
+
--- frameworks/runtime-src/Classes/src/map.c
+++ frameworks/runtime-src/Classes/src/map.c
@@ -1,20 +1,21 @@
 #include "map.h"
 bool
-	map_load(struct map *map, const char* filename){
+map_load(struct map *map, const char* filename){
+		
 	return true;
 }
 
 bool
-	map_canmove(struct map *map, const uint16_t x, const uint16_t y){
+map_canmove(struct map *map, const uint16_t x, const uint16_t y){
 	return *(map->flags + y * map->header.width + x) & 0x01 == 0;
 }
 
 bool
-	map_canflay(struct map *map, const uint16_t x, const uint16_t y){
+map_canflay(struct map *map, const uint16_t x, const uint16_t y){
 	return *(map->flags + y * map->header.width + x) & 0x02 == 0;
 }
 
 bool
-	map_blend(struct map *map, const uint16_t x, const uint16_t y){
+map_blend(struct map *map, const uint16_t x, const uint16_t y){
 	return *(map->flags + y * map->header.width + x) & 0x03 == 0;
 }
